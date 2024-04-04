c356189bc38bd9493837b334613321563f61e6ec
FarukIm <f.imamovic27@gmail.com>
2024-04-04 19:49:41 +0200

    CB-56: added circular and square button components

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -7,9 +7,21 @@
     <Button color="secondary-fill" size="md">malo duze dugme</Button>
     <Button color="primary-outline" size="lg">malo duze dugme</Button>
     <Button color="secondary-outline" size="xl">malo duze dugme</Button>
+
+    <CircleButton color="primary-fill" size="sm">X</CircleButton>
+    <CircleButton color="light-outline" size="md">X</CircleButton>
+    <CircleButton color="normal-outline" size="lg">X</CircleButton>
+    <CircleButton color="dark-outline" size="xl">X</CircleButton>
+
+    <SquareButton color="primary" size="sm">X</SquareButton>
+    <SquareButton color="primary" size="md">X</SquareButton>
+    <SquareButton color="secondary" size="lg">X</SquareButton>
+    <SquareButton color="secondary" size="xl">X</SquareButton>
   </div>
 </template>
 
 <script setup lang="ts">
 import Button from "../components/shared/Button.vue";
+import CircleButton from "../components/shared/CircleButton.vue";
+import SquareButton from "../components/shared/SquareButton.vue";
 </script>
